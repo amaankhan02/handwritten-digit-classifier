cebcac4baba3f8173ef6060dd606ed499a66ca32
Amaan Khan <amaanite@gmail.com>
2021-04-10 17:46:46 -0500

    Wrote pipeline to train model and compute accuracy on test data

--- apps/train_model_main.cc
+++ apps/train_model_main.cc
@@ -3,24 +3,23 @@
 #include <string>
 #include <tuple>
 
-//#include <core/dataset.h>
 #include <core/model.h>
 
 using naivebayes::core::Dataset;
 using naivebayes::core::Model;
 
 int main() {
-  // load dataset from file
-  Dataset dataset = Dataset(28, 28);
+  // load train_dataset from file
+  Dataset train_dataset = Dataset(28, 28);
   std::string input_path;
   std::ifstream input_file(input_path);
   if (input_file.is_open()) {
-    input_file >> dataset;
+    input_file >> train_dataset;
   }
 
-  // construct model and train
+  // construct model and train // TODO: remove these magic numbers!
   Model model = Model(28, 28, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 1);
-  model.Train(dataset.GetImages(), dataset.GetLabels());
+  model.Train(train_dataset.GetImages(), train_dataset.GetLabels());
 
   // save model features to file
   std::string output_path;
@@ -28,6 +27,17 @@ int main() {
   if (output_file.is_open()) {
     output_file << model;
   }
+
+  // load test data
+  std::string test_data_path;
+  std::ifstream test_data_file(test_data_path);
+  Dataset test_dataset = Dataset(28, 28);
+  if (test_data_file.is_open()) {
+    test_data_file >> test_dataset;
+  }
+
+  float accuracy = model.ComputeAccuracy(test_dataset.GetImages(), test_dataset.GetLabels());
+  std::cout << "Model Accuracy: " << accuracy;
 
   return 0;
 }
