cb34e3520b3dbc34b7ce5ac7a772b6045fdd7866
Amaan Khan <amaanite@gmail.com>
2021-04-13 17:55:51 -0500

    Changed file paths and made minor style changes

--- tests/test_dataset.cc
+++ tests/test_dataset.cc
@@ -11,7 +11,7 @@ using naivebayes::core::Pixel;
 using std::vector;
 
 TEST_CASE("Test >> Operator with 3 images") {
-  std::ifstream input_file(R"(../../../data/test_dataset_data.txt)");
+  std::ifstream input_file(R"(test_data/test_dataset_data.txt)");
   Dataset dataset = Dataset(3, 3);
   if (input_file.is_open()) {
     input_file >> dataset;
@@ -24,8 +24,7 @@ TEST_CASE("Test >> Operator with 3 images") {
   SECTION("Check pixel values are correct (parsed correctly)") {
     vector<vector<Pixel>> expected_img12 = {
         {Pixel::kWhite, Pixel::kWhite, Pixel::kWhite},
-        {Pixel::kBlack, Pixel::kBlack,
-         Pixel::kBlack},  // make sure gray is parsed as black
+        {Pixel::kBlack, Pixel::kBlack, Pixel::kBlack},
         {Pixel::kBlack, Pixel::kBlack, Pixel::kBlack}};
     vector<vector<Pixel>> expected_img3 = {
         {Pixel::kBlack, Pixel::kBlack, Pixel::kBlack},
@@ -58,7 +57,7 @@ TEST_CASE("Test >> Operator with 3 images") {
 TEST_CASE("Test >> Operator with images invalid size/inputs") {
   SECTION("Check it throws error for num COLS don't match the input width") {
     std::ifstream input_file(
-        R"(../../../data/test_dataset_invalidcolssizedata.txt)");
+        R"(test_data/test_dataset_invalidcolssizedata.txt)");
     Dataset dataset = Dataset(3, 3);
     if (input_file.is_open()) {
       REQUIRE_THROWS_AS(input_file >> dataset, std::invalid_argument);
@@ -67,7 +66,7 @@ TEST_CASE("Test >> Operator with images invalid size/inputs") {
 
   SECTION("Check it throws error for num ROWS don't match the input height") {
     std::ifstream input_file(
-        R"(../../../data/test_dataset_invalidrowssizedata.txt)");
+        R"(test_data/test_dataset_invalidrowssizedata.txt)");
     Dataset dataset = Dataset(3, 3);
     if (input_file.is_open()) {
       REQUIRE_THROWS_AS(input_file >> dataset, std::invalid_argument);
@@ -75,8 +74,7 @@ TEST_CASE("Test >> Operator with images invalid size/inputs") {
   }
 
   SECTION("Check it throws error for invalid CHARS in the image") {
-    std::ifstream input_file(
-        R"(../../../data/test_dataset_data_invalidchars.txt)");
+    std::ifstream input_file(R"(test_data/test_dataset_data_invalidchars.txt)");
     Dataset dataset = Dataset(3, 3);
     if (input_file.is_open()) {
       REQUIRE_THROWS_AS(input_file >> dataset, std::invalid_argument);
@@ -86,7 +84,7 @@ TEST_CASE("Test >> Operator with images invalid size/inputs") {
 
 TEST_CASE(
     "Test >> Operator with 5x5 image size and different image count in file") {
-  std::ifstream input_file(R"(../../../data/test_dataset_data_5x5.txt)");
+  std::ifstream input_file(R"(test_data/test_dataset_data_5x5.txt)");
   Dataset dataset = Dataset(5, 5);
   if (input_file.is_open()) {
     input_file >> dataset;
